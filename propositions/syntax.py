772fada04ecbbae5881983171873c45020ddef8b
amilion <amirrezaahadzadeh1@gmail.com>
2023-05-11 14:11:47 +0330

    finish synthesize function!

---
+++ propositions/syntax.py
@@ -0,0 +1,385 @@
+# This file is part of the materials accompanying the book
+# "Mathematical Logic through Python" by Gonczarowski and Nisan,
+# Cambridge University Press. Book site: www.LogicThruPython.org
+# (c) Yannai A. Gonczarowski and Noam Nisan, 2017-2022
+# File name: propositions/syntax.py
+
+"""Syntactic handling of propositional formulas."""
+
+from __future__ import annotations
+from functools import lru_cache
+from typing import Mapping, Optional, Set, Tuple, Union
+
+from logic_utils import frozen, memoized_parameterless_method
+
+
+@lru_cache(maxsize=100)  # Cache the return value of is_variable
+def is_variable(string: str) -> bool:
+    """Checks if the given string is a variable name.
+
+    Parameters:
+        string: string to check.
+
+    Returns:
+        ``True`` if the given string is a variable name, ``False`` otherwise.
+    """
+    return string[0] >= 'p' and string[0] <= 'z' and \
+        (len(string) == 1 or string[1:].isdecimal())
+
+
+@lru_cache(maxsize=100)  # Cache the return value of is_constant
+def is_constant(string: str) -> bool:
+    """Checks if the given string is a constant.
+
+    Parameters:
+        string: string to check.
+
+    Returns:
+        ``True`` if the given string is a constant, ``False`` otherwise.
+    """
+    return string == 'T' or string == 'F'
+
+
+@lru_cache(maxsize=100)  # Cache the return value of is_unary
+def is_unary(string: str) -> bool:
+    """Checks if the given string is a unary operator.
+
+    Parameters:
+        string: string to check.
+
+    Returns:
+        ``True`` if the given string is a unary operator, ``False`` otherwise.
+    """
+    return string == '~'
+
+
+@lru_cache(maxsize=100)  # Cache the return value of is_binary
+def is_binary(string: str) -> bool:
+    """Checks if the given string is a binary operator.
+
+    Parameters:
+        string: string to check.
+
+    Returns:
+        ``True`` if the given string is a binary operator, ``False`` otherwise.
+    """
+    return string == '&' or string == '|' or string == '->'
+    # For Chapter 3:
+    # return string in {'&', '|',  '->', '+', '<->', '-&', '-|'}
+
+
+@lru_cache(maxsize=100)
+def parse_str(string: str) -> Tuple(str, str):
+    """Parsed first valid representation of formula or first token.
+
+    Parameters:
+        string: string to parse.
+
+    Returns:
+        (first valid representation of formula or first token, rest) 
+    """
+    if is_variable(string[0]):
+        decimals = ""
+        for i in string[1:]:
+            if (i.isdecimal()):
+                decimals += i
+            else:
+                break
+        len_decimals = len(decimals)
+        return string[:len_decimals+1], string[len_decimals+1:]
+    elif string[:2] == "->":
+        return string[:2], string[2:]
+    elif string[0] == '(':
+        counter = 0
+        for ind, ele in enumerate(string):
+            if ele == '(':
+                counter += 1
+            elif ele == ')':
+                counter -= 1
+            elif counter == 0:
+                break
+        else:
+            ind += 1
+        if counter:
+            return string[0], string[1:]
+        return string[:ind], string[ind:]
+    else:
+        return string[0], string[1:]
+
+
+@frozen
+class Formula:
+    """An immutable propositional formula in tree representation, composed from
+    variable names, and operators applied to them.
+
+    Attributes:
+        root (`str`): the constant, variable name, or operator at the root of
+            the formula tree.
+        first (`~typing.Optional`\\[`Formula`]): the first operand of the root,
+            if the root is a unary or binary operator.
+        second (`~typing.Optional`\\[`Formula`]): the second operand of the
+            root, if the root is a binary operator.
+    """
+    root: str
+    first: Optional[Formula]
+    second: Optional[Formula]
+
+    def __init__(self, root: str, first: Optional[Formula] = None,
+                 second: Optional[Formula] = None):
+        """Initializes a `Formula` from its root and root operands.
+
+        Parameters:
+            root: the root for the formula tree.
+            first: the first operand for the root, if the root is a unary or
+                binary operator.
+            second: the second operand for the root, if the root is a binary
+                operator.
+        """
+        if is_variable(root) or is_constant(root):
+            assert first is None and second is None
+            self.root = root
+        elif is_unary(root):
+            assert first is not None and second is None
+            self.root, self.first = root, first
+        else:
+            assert is_binary(root)
+            assert first is not None and second is not None
+            self.root, self.first, self.second = root, first, second
+
+    @memoized_parameterless_method
+    def __repr__(self) -> str:
+        """Computes the string representation of the current formula.
+
+        Returns:
+            The standard string representation of the current formula.
+        """
+        # Task 1.1
+        if is_variable(self.root) or is_constant(self.root):
+            return str(self.root)
+        elif is_unary(self.root):
+            return str(self.root) + Formula.__repr__(self.first)
+        elif is_binary(self.root):
+            return '(' + Formula.__repr__(self.first) + str(self.root) + Formula.__repr__(self.second) + ')'
+
+    def __eq__(self, other: object) -> bool:
+        """Compares the current formula with the given one.
+
+        Parameters:
+            other: object to compare to.
+
+        Returns:
+            ``True`` if the given object is a `Formula` object that equals the
+            current formula, ``False`` otherwise.
+        """
+        return isinstance(other, Formula) and str(self) == str(other)
+
+    def __ne__(self, other: object) -> bool:
+        """Compares the current formula with the given one.
+
+        Parameters:
+            other: object to compare to.
+
+        Returns:
+            ``True`` if the given object is not a `Formula` object or does not
+            equal the current formula, ``False`` otherwise.
+        """
+        return not self == other
+
+    def __hash__(self) -> int:
+        return hash(str(self))
+
+    @memoized_parameterless_method
+    def variables(self) -> Set[str]:
+        """Finds all variable names in the current formula.
+
+        Returns:
+            A set of all variable names used in the current formula.
+        """
+        # Task 1.2
+        if is_variable(self.root):
+            return {str(self.root)}
+        elif is_constant(self.root):
+            return set()
+        elif is_unary(self.root):
+            return Formula.variables(self.first)
+        elif is_binary(self.root):
+            return Formula.variables(self.first).union(Formula.variables(self.second))
+
+    @memoized_parameterless_method
+    def operators(self) -> Set[str]:
+        """Finds all operators in the current formula.
+
+        Returns:
+            A set of all operators (including ``'T'`` and ``'F'``) used in the
+            current formula.
+        """
+        # Task 1.3
+        if is_variable(self.root):
+            return set()
+        elif is_constant(self.root):
+            return {str(self.root)}
+        elif is_unary(self.root):
+            return {str(self.root)}.union(Formula.operators(self.first))
+        elif is_binary(self.root):
+            return {str(self.root)}.union(Formula.operators(self.first), Formula.operators(self.second))
+
+    @staticmethod
+    def _parse_prefix(string: str) -> Tuple[Union[Formula, None], str]:
+        """Parses a prefix of the given string into a formula.
+
+        Parameters:
+            string: string to parse.
+
+        Returns:
+            A pair of the parsed formula and the unparsed suffix of the string.
+            If the given string has as a prefix a variable name (e.g.,
+            ``'x12'``) or a unary operator followed by a variable name, then the
+            parsed prefix will include that entire variable name (and not just a
+            part of it, such as ``'x1'``). If no prefix of the given string is a
+            valid standard string representation of a formula then returned pair
+            should be of ``None`` and an error message, where the error message
+            is a string with some human-readable content.
+        """
+        # Task 1.4
+        if not string:  # string shouldn't be empty
+            return None, "string not found!"
+        # parses string to first representation of formula or first token and rest of string
+        first, rest = parse_str(string)
+        if is_variable(first) or is_constant(first):
+            return Formula(first), rest
+        elif is_binary(first):
+            return None, string
+        elif is_unary(first):
+            if not rest:
+                return None, first + " is unary not a valid formula!"
+            return Formula(first, Formula._parse_prefix(rest)[0]), Formula._parse_prefix(rest)[1]
+        elif first[0] == '(':
+            if first == '(':
+                return None, "some of \'(\' are not closed!"
+            first_f, _rest = Formula._parse_prefix(first[1:-1])
+            if not _rest:
+                return None, "there should be two formulas but one found!"
+            connective, _second = parse_str(_rest)
+            if not is_binary(connective):
+                return None, "wrong connective is been used!"
+            second_f, second_r = Formula._parse_prefix(_second)
+            if second_r:
+                if not second_f:
+                    return None, f"{second_r} is not a valid formula!"
+                return None, f"couldn\'t parse {second_r}!"
+            return Formula(connective, first_f, second_f), rest
+        else:
+            return None, string + " is not a valid formula!"
+
+    @staticmethod
+    def is_formula(string: str) -> bool:
+        """Checks if the given string is a valid representation of a formula.
+
+        Parameters:
+            string: string to check.
+
+        Returns:
+            ``True`` if the given string is a valid standard string
+            representation of a formula, ``False`` otherwise.
+        """
+        # Task 1.5
+        error = Formula._parse_prefix(string)[1]
+        if error:  # if an error accures will return false
+            return False
+        return True
+
+    @staticmethod
+    def parse(string: str) -> Formula:
+        """Parses the given valid string representation into a formula.
+
+        Parameters:
+            string: string to parse.
+
+        Returns:
+            A formula whose standard string representation is the given string.
+        """
+        assert Formula.is_formula(string)
+        return Formula._parse_prefix(string)[0]  # returns parsed formula
+        # Task 1.6
+
+    def polish(self) -> str:
+        """Computes the polish notation representation of the current formula.
+
+        Returns:
+            The polish notation representation of the current formula.
+        """
+        # Optional Task 1.7
+        # should return all self.roots
+        if is_variable(self.root) or is_constant(self.root):
+            return self.root
+        elif is_unary(self.root):
+            return self.root + Formula.polish(self.first)
+        elif is_binary(self.root):
+            return self.root + Formula.polish(self.first) + Formula.polish(self.second)
+
+    @staticmethod
+    def parse_polish(string: str) -> Formula:
+        """Parses the given polish notation representation into a formula.
+
+        Parameters:
+            string: string to parse.
+
+        Returns:
+            A formula whose polish notation representation is the given string.
+        """
+        # Optional Task 1.8
+
+    def substitute_variables(self, substitution_map: Mapping[str, Formula]) -> \
+            Formula:
+        """Substitutes in the current formula, each variable name `v` that is a
+        key in `substitution_map` with the formula `substitution_map[v]`.
+
+        Parameters:
+            substitution_map: mapping defining the substitutions to be
+                performed.
+
+        Returns:
+            The formula resulting from performing all substitutions. Only
+            variable name occurrences originating in the current formula are
+            substituted (i.e., variable name occurrences originating in one of
+            the specified substitutions are not subjected to additional
+            substitutions).
+
+        Examples:
+            >>> Formula.parse('((p->p)|r)').substitute_variables(
+            ...     {'p': Formula.parse('(q&r)'), 'r': Formula.parse('p')})
+            (((q&r)->(q&r))|p)
+        """
+        for variable in substitution_map:
+            assert is_variable(variable)
+        # Task 3.3
+
+    def substitute_operators(self, substitution_map: Mapping[str, Formula]) -> \
+            Formula:
+        """Substitutes in the current formula, each constant or operator `op`
+        that is a key in `substitution_map` with the formula
+        `substitution_map[op]` applied to its (zero or one or two) operands,
+        where the first operand is used for every occurrence of ``'p'`` in the
+        formula and the second for every occurrence of ``'q'``.
+
+        Parameters:
+            substitution_map: mapping defining the substitutions to be
+                performed.
+
+        Returns:
+            The formula resulting from performing all substitutions. Only
+            operator occurrences originating in the current formula are
+            substituted (i.e., operator occurrences originating in one of the
+            specified substitutions are not subjected to additional
+            substitutions).
+
+        Examples:
+            >>> Formula.parse('((x&y)&~z)').substitute_operators(
+            ...     {'&': Formula.parse('~(~p|~q)')})
+            ~(~~(~x|~y)|~~z)
+        """
+        for operator in substitution_map:
+            assert is_constant(operator) or is_unary(operator) or \
+                is_binary(operator)
+            assert substitution_map[operator].variables().issubset({'p', 'q'})
+        # Task 3.4
